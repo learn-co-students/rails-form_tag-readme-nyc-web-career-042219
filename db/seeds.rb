2d78d92cb8e8f4981cb294e598a81f2a3bd090f4
Jennifer Oh <ohjennifer.90@gmail.com>
2019-05-16 17:00:00 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "Hello", description: "Hello World. It's Jennifer")
+Post.create(title: "Goodbye", description: "Goodbye World. It's Jennifer")
